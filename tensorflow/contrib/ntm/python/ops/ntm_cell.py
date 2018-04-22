595dce44f5e8115d5edfc42fef0043479614d118
Jonathan Gill <jtgill@email.arizona.edu>
2018-04-22 00:29:36 -0700

    Build and import updates
    Updated bazel BUILD file to get TF to compile. Updated RNNCell import statement in ntm_cell.

--- tensorflow/contrib/ntm/python/ops/ntm_cell.py
+++ tensorflow/contrib/ntm/python/ops/ntm_cell.py
@@ -16,7 +16,8 @@ from tensorflow.python.ops import linalg_ops
 from tensorflow.python.ops import math_ops
 from tensorflow.python.ops import nn_ops
 from tensorflow.python.ops import variable_scope as vs
-from tensorflow.python.ops.rnn_cell_impl import _RNNCell as RNNCell
+#from tensorflow.python.ops.rnn_cell_impl import _RNNCell as RNNCell
+from tensorflow.python.ops.rnn_cell_impl import RNNCell
 
 
 # TODO(@therealjtgill) Add a note in the code stating how variable names map
